c7410ea91b639ae47c6f73b5caf65a644811bcb7
Flaaim <Flaaim@list.ru>
2026-09-14 16:30:17 +1000

    added role admin to profile fixture

--- api/src/Profile/Fixture/ProfileFixture.php
+++ api/src/Profile/Fixture/ProfileFixture.php
@@ -6,10 +6,12 @@ namespace App\Profile\Fixture;
 
 use App\Profile\Entity\Profile\Email;
 use App\Profile\Entity\Profile\ProfileId;
+use App\Profile\Entity\Profile\Role;
 use App\Profile\Test\Builder\ProfileBuilder;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Persistence\ObjectManager;
 
+/** @psalm-suppress UnusedClass */
 final class ProfileFixture extends AbstractFixture
 {
     public function load(ObjectManager $manager): void
@@ -17,7 +19,9 @@ final class ProfileFixture extends AbstractFixture
         $profile = new ProfileBuilder()
             ->withProfileId(new ProfileId('eaa3e157-5017-4d01-84f7-3275f8e4492e'))
             ->withEmail(new Email('flaaim@list.ru'))
+            ->withRole(Role::admin())
             ->build();
+
         $manager->persist($profile);
 
         $manager->flush();
